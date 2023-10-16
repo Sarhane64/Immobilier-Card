530a7031afd89264a907b4b89bfd23ab7a54bf82
jean519 <129189905+jean519@users.noreply.github.com>
2023-10-16 10:17:26 +0200

    ajout de la DataBase des prpriete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,70 @@
 const Card = () => {
   const array = [
     {
-        image:null,
-        prix:null,
-        description:null,
-        adresse:null,
-        like:0
+      image: null,
+      prix: 350000,
+      description: "Charmante maison de campagne avec jardin spacieux et vue imprenable sur les collines environnantes. La propriété comprend quatre chambres, deux salles de bains et une cuisine entièrement équipée. Parfait pour les amoureux de la nature et de la tranquillité.",
+      adresse: "123 Rue de la Campagne, 75000 Paris, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 620000,
+      description: "Appartement lumineux avec vue panoramique sur la ville. Il dispose de trois chambres spacieuses, d'une cuisine moderne et d'un salon ouvert. Situé dans un quartier animé avec des boutiques et des restaurants à proximité.",
+      adresse: "456 Avenue du Soleil, 69000 Lyon, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 450000,
+      description: "Spacieuse maison de ville rénovée avec un charmant jardin privé. La maison dispose de cinq chambres, d'un grand salon et d'une salle à manger élégante. Idéalement située près des écoles et des parcs.",
+      adresse: "789 Rue de la Paix, 33000 Bordeaux, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 280000,
+      description: "Appartement confortable dans un quartier calme. Deux chambres, une cuisine entièrement équipée et un balcon offrant une vue magnifique sur les environs verdoyants. À proximité des transports en commun et des commodités.",
+      adresse: "101 Rue du Bonheur, 59000 Lille, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 720000,
+      description: "Maison moderne avec piscine et vue sur la mer. Quatre chambres spacieuses, un salon élégant et une cuisine design. Profitez du style de vie méditerranéen dans cette magnifique propriété.",
+      adresse: "555 Chemin de la Mer, 06000 Nice, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 380000,
+      description: "Appartement récemment rénové dans un quartier animé. Trois chambres lumineuses, une cuisine moderne et un salon confortable. Idéal pour les jeunes professionnels et les familles.",
+      adresse: "222 Boulevard des Arts, 44000 Nantes, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 520000,
+      description: "Villa spacieuse avec jardin paysager. Cinq chambres élégantes, un grand salon et une cuisine entièrement équipée. Profitez d'une intimité totale dans ce havre de paix situé à proximité des commodités.",
+      adresse: "777 Avenue de la Tranquillité, 25000 Besançon, France",
+      like: null,
+      pro: null
+    },
+    {
+      image: null,
+      prix: 290000,
+      description: "Appartement en duplex avec vue sur la rivière. Deux chambres confortables, une terrasse privée et un accès facile aux sentiers de randonnée environnants. Vivez au plus près de la nature dans ce magnifique refuge.",
+      adresse: "909 Rue de la Rivière, 80000 Amiens, France",
+      like: null,
+      pro: null
     }
-  ];
+  ]; return array
 };
 export default Card;
+
--- src/components/Db.jsx
+++ src/components/Db.jsx
@@ -1,9 +1,70 @@
 const Db = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    const array = [
+        {
+          image: "https://img.paruvendu.fr/media_ext/photos.ubiflow.net/bc/49/LzM0MDM2OS8zODY3NjM3NDAvcGhvdG9zLzEuanBnPzIwMjMxMDE2MDAwMzIy_rct?func=crop&w=1000&gravity=auto",
+          prix: 350000,
+          description: "Charmante maison de campagne avec jardin spacieux et vue imprenable sur les collines environnantes. La propriété comprend quatre chambres, deux salles de bains et une cuisine entièrement équipée. Parfait pour les amoureux de la nature et de la tranquillité.",
+          adresse: "123 Rue de la Campagne, 75000 Paris, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://img.paruvendu.fr/media_ext/_https_/media.apimo.pro/c5/ab/L3BpY3R1cmUvODM0MDcvODM0MDY2MjgvNTI1NzE4MTI1NjUxZWMxM2FhM2YwZTcuNTI1NTEyMzhfNDBhYjY1ZWI1MF8xOTIwLndlYnAtb3JpZ2luYWwuanBnPzE2OTY1MTQzNjImMjAyMzEwMTQwMDI4NTg_rct?func=crop&w=1000&gravity=auto",
+          prix: 620000,
+          description: "Appartement lumineux avec vue panoramique sur la ville. Il dispose de trois chambres spacieuses, d'une cuisine moderne et d'un salon ouvert. Situé dans un quartier animé avec des boutiques et des restaurants à proximité.",
+          adresse: "456 Avenue du Soleil, 69000 Lyon, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://img.paruvendu.fr/media_ext/_https_/www.human-immobilier.fr/10/99/L2ltYWdlcy84NS0yNDAwXzAxMDYyMzA2NTMwMi5qcGc_MjAyMzEwMTUwODA1MTA_rct?func=crop&w=1000&gravity=auto",
+          prix: 450000,
+          description: "Spacieuse maison de ville rénovée avec un charmant jardin privé. La maison dispose de cinq chambres, d'un grand salon et d'une salle à manger élégante. Idéalement située près des écoles et des parcs.",
+          adresse: "789 Rue de la Paix, 33000 Bordeaux, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://img.paruvendu.fr/media_ext/photos.ubiflow.net/8a/37/LzQ0MDQxNC80MDExMDA2NjkvcGhvdG9zLzEuanBnPzIwMjMxMDE1MDk1ODE1_rct?func=crop&w=1000&gravity=auto",
+          prix: 280000,
+          description: "Appartement confortable dans un quartier calme. Deux chambres, une cuisine entièrement équipée et un balcon offrant une vue magnifique sur les environs verdoyants. À proximité des transports en commun et des commodités.",
+          adresse: "101 Rue du Bonheur, 59000 Lille, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://media.paruvendu.fr/media_ext/_https_/media.paruvendu.fr/2b/ea/L21lZGlhX3ByZy9BRExOMjI2L3ByZ19waG90XzBfcF81NTk1NjQwXzdhNmVlNWQxOWI1MmI5ZDY5ZGM4ZTMzMzVlZGM2YjdmN2RhZWMyMzNhOTU5YjJjYWZhYzhjNzlmODdjOTE2ODkxNjk4MTM5MDAtanBnPzIwMjMtMTAtMTYgMDc6MDA6MjYuODgw_sizecrop_1000x1000?2023101610",
+          prix: 720000,
+          description: "Maison moderne avec piscine et vue sur la mer. Quatre chambres spacieuses, un salon élégant et une cuisine design. Profitez du style de vie méditerranéen dans cette magnifique propriété.",
+          adresse: "555 Chemin de la Mer, 06000 Nice, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://img.paruvendu.fr/media_ext/_https_/media.paruvendu.fr/3d/42/L21lZGlhLXBhL1dJMTcvMS85L1dJMTcxOTk4MjA5XzEuanBlZz8yMDIzMTAwODE5NTE1Ng_rct?func=crop&w=1000&gravity=auto",
+          prix: 380000,
+          description: "Appartement récemment rénové dans un quartier animé. Trois chambres lumineuses, une cuisine moderne et un salon confortable. Idéal pour les jeunes professionnels et les familles.",
+          adresse: "222 Boulevard des Arts, 44000 Nantes, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://img.paruvendu.fr/media_ext/photos.ubiflow.net/40/3a/LzM0MDM2OS8zOTY5MzUzODEvcGhvdG9zLzEuanBnPzIwMjMxMDEyMDUwNTEy_rct?func=crop&w=1000&gravity=auto",
+          prix: 520000,
+          description: "Villa spacieuse avec jardin paysager. Cinq chambres élégantes, un grand salon et une cuisine entièrement équipée. Profitez d'une intimité totale dans ce havre de paix situé à proximité des commodités.",
+          adresse: "777 Avenue de la Tranquillité, 25000 Besançon, France",
+          like: null,
+          pro: null
+        },
+        {
+          image: "https://img.paruvendu.fr/media_ext/_https_/www.human-immobilier.fr/d7/fa/L2ltYWdlcy84NS0yMzUzXzE5MTIyMjA4NTMxNy5qcGc_MjAyMzEwMTUwODA1MjE_rct?func=crop&w=1000&gravity=auto",
+          prix: 290000,
+          description: "Appartement en duplex avec vue sur la rivière. Deux chambres confortables, une terrasse privée et un accès facile aux sentiers de randonnée environnants. Vivez au plus près de la nature dans ce magnifique refuge.",
+          adresse: "909 Rue de la Rivière, 80000 Amiens, France",
+          like: null,
+          pro: null
+        }
+      ]; return array
+}
 
 export default Db;
